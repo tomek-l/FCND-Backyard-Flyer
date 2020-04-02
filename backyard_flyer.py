03c49f59fbea3d209cd293b93e609a8ddb2be2b6
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-04-02 01:00:56 -0700

    formatted with black

--- backyard_flyer.py
+++ backyard_flyer.py
@@ -18,8 +18,8 @@ class States(Enum):
     LANDED = 4
     DISARMED = 5
 
-class BackyardFlyer(Drone):
 
+class BackyardFlyer(Drone):
     def __init__(self, connection):
         super().__init__(connection)
         self.in_mission = True
@@ -29,7 +29,7 @@ class BackyardFlyer(Drone):
         self.target_position = self.wp_list[0]
         self.state = States.MANUAL
         self.waypoints_completed = False
-        self.eps = 0.5 # m
+        self.eps = 0.5  # m
 
         # initial state
         self.flight_state = States.MANUAL
@@ -41,17 +41,18 @@ class BackyardFlyer(Drone):
 
     @staticmethod
     def euc_dist(a, b):
-        sumsq = (a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]+b[2])**2 
+        sumsq = (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] + b[2]) ** 2
         return math.sqrt(sumsq)
 
     @staticmethod
     def calculate_box(size=10, altitude=3):
         return [
-            [0,0, altitude],
-            [size,0, altitude],
-            [size,size, altitude],
-            [0,size, altitude],
-            [0,0, altitude]]
+            [0, 0, altitude],
+            [size, 0, altitude],
+            [size, size, altitude],
+            [0, size, altitude],
+            [0, 0, altitude],
+        ]
 
     def local_position_callback(self):
         # wasn't needed
@@ -80,9 +81,9 @@ class BackyardFlyer(Drone):
 
     def arming_transition(self):
         self.take_control()
-        self.set_home_position(self.global_position[0],
-                        self.global_position[1],
-                        self.global_position[2])
+        self.set_home_position(
+            self.global_position[0], self.global_position[1], self.global_position[2]
+        )
 
         self.arm()
         self.state = States.ARMED
@@ -97,27 +98,33 @@ class BackyardFlyer(Drone):
 
     def waypoint_transition(self):
 
-        if self.euc_dist(self.local_position, self.target_position) < self.eps: # if reached waypoint
-            if self.wp_idx+1 == len(self.wp_list): # if reached last waypoint
-                pass # let landing_transition take care of this 
+        if (
+            self.euc_dist(self.local_position, self.target_position) < self.eps
+        ):  # if reached waypoint
+            if self.wp_idx + 1 == len(self.wp_list):  # if reached last waypoint
+                pass  # let landing_transition take care of this
             else:
                 self.wp_idx += 1
                 self.target_position = self.wp_list[self.wp_idx]
                 self.cmd_position(*self.target_position, heading=0)
                 print(f"set target to {self.target_position}")
-                self.state = States.WAYPOINTS # stay in waypoint
+                self.state = States.WAYPOINTS  # stay in waypoint
         print("waypoint transition")
 
     def landing_transition(self):
-        if self.euc_dist(self.local_position, self.target_position) < self.eps: # if reached waypoint
-            if self.wp_idx+1 == len(self.wp_list): # if reached last waypoint
+        if (
+            self.euc_dist(self.local_position, self.target_position) < self.eps
+        ):  # if reached waypoint
+            if self.wp_idx + 1 == len(self.wp_list):  # if reached last waypoint
                 self.land()
                 self.state = States.LANDED
-                self.target_position = [0,0,0]
+                self.target_position = [0, 0, 0]
         print("landing transition")
 
     def disarming_transition(self):
-        if self.euc_dist(self.local_position, self.target_position) < self.eps: # if reached waypoint
+        if (
+            self.euc_dist(self.local_position, self.target_position) < self.eps
+        ):  # if reached waypoint
             self.disarm()
             self.state = States.DISARMED
         print("disarm transition")
@@ -146,12 +153,16 @@ class BackyardFlyer(Drone):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', type=int, default=5760, help='Port number')
-    parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
+    parser.add_argument("--port", type=int, default=5760, help="Port number")
+    parser.add_argument(
+        "--host", type=str, default="127.0.0.1", help="host address, i.e. '127.0.0.1'"
+    )
     args = parser.parse_args()
 
-    conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), threaded=False, PX4=False)
-    #conn = WebSocketConnection('ws://{0}:{1}'.format(args.host, args.port))
+    conn = MavlinkConnection(
+        "tcp:{0}:{1}".format(args.host, args.port), threaded=False, PX4=False
+    )
+    # conn = WebSocketConnection('ws://{0}:{1}'.format(args.host, args.port))
     drone = BackyardFlyer(conn)
     time.sleep(2)
     drone.start()
